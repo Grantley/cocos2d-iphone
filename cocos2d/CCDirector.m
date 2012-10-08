4250049eedaefecfd94c36b9673b41740de7aca3
Grantley Day <grantley.day@gg-prod.com>
2012-10-08 12:50:15 +1100

    Slight Changes to Stat Display

--- cocos2d/CCDirector.m
+++ cocos2d/CCDirector.m
@@ -26,6 +26,8 @@
 
 /* Idea of decoupling Window from Director taken from OC3D project: http://code.google.com/p/oc3d/
  */
+#import <mach/mach.h>
+#import <mach/mach_host.h>
 
 #import <unistd.h>
 
@@ -586,9 +588,31 @@ static CCDirector *_sharedDirector = nil;
 			[FPSLabel_ setString:fpsstr];
 			[fpsstr release];
 			
-			NSString *draws = [[NSString alloc] initWithFormat:@"%4lu", (unsigned long)__ccNumberOfDraws];
+            // Display Memory Usage instead
+            mach_port_t host_port;
+            mach_msg_type_number_t host_size;
+            vm_size_t pagesize;
+            host_port = mach_host_self();
+            host_size = sizeof(vm_statistics_data_t) / sizeof(integer_t);
+            host_page_size(host_port, &pagesize);
+            vm_statistics_data_t vm_stat;
+            
+            float mem_free;
+            float mem_use;
+            if (host_statistics(host_port, HOST_VM_INFO, (host_info_t)&vm_stat, &host_size) != KERN_SUCCESS) {
+                NSLog(@"Failed to fetch vm statistics");
+            }
+            else {
+                mem_free = ((float)vm_stat.free_count * (float)pagesize) / 1024000;
+                mem_use = ((float)(vm_stat.active_count +  vm_stat.inactive_count + vm_stat.wire_count) * (float)pagesize) / 1024000;
+            }
+            
+			//NSString *draws = [[NSString alloc] initWithFormat:@"%4lu", (unsigned long)__ccNumberOfDraws];
+            
+            NSString *draws = [[NSString alloc] initWithFormat:@"%0.2f %0.2f", mem_use, mem_free];
 			[drawsLabel_ setString:draws];
 			[draws release];
+             
 		}
 
 		[drawsLabel_ visit];
